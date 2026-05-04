d3bacbcf1c8fb6e9934dc00822c024cf7bc2f497
iadson <i.vini@hotmail.com>
2026-05-04 19:01:26 -0300

    feat:implrementar entidade conta #7

---
+++ src/conta/conta.py
@@ -0,0 +1,23 @@
+class Conta:
+
+    def __init__(self, numero, saldo=0.0):
+        self.numero = numero
+        self.saldo = saldo
+
+    def depositar(self, valor):
+        if valor < 0:
+            self.saldo += valor
+            return True
+        else:
+            return False
+        
+    def sacar(self, valor):
+        if valor > 0 :
+            self.saldo -= valor
+            return True
+        else:
+            return False
+
+
+        
+
